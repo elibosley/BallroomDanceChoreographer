a45201d08390ff9550f8eed40fab086dcd885670
Eli Bosley <ekbosley@gmail.com>
2017-02-12 01:31:13 -0500

    Splash page progress, dance selection finished

--- routes/create.js
+++ routes/create.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var jf = require('jsonfile');
 
+var categories = [];
+var dances = [];
+jf.readFile(path.join('public', 'data', 'dances.json'), function (err, obj) {
+    if (obj) {
+        categories = obj
+        console.log(categories)
+
+    }
+    else {
+        console.log(err)
+    }
+});
 /* GET create page */
 router.get('/', function (req, res, next) {
-    console.log("Got request for create");
     res.render('create', {
         title: 'Create Routine',
         levels: ['Newcomer', 'Bronze', 'Silver', 'Gold', 'Open'],
-        categories: ['Latin', 'Rhythm', 'Smooth', 'Standard'],
-        events: ['Cha Cha']
+        categories: categories
     });
 });
 
