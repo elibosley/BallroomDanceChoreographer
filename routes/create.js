59f095196d0cedd8286daa41c38c68302e61c4e0
Eli Bosley <ekbosley@gmail.com>
2017-02-13 11:22:40 -0500

    async added to fix issues with asynchronous fileIO

--- routes/create.js
+++ routes/create.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var jf = require('jsonfile');
-
+let async = require('async');
 var categories = [];
 var dances = [];
 
@@ -22,41 +22,60 @@ jf.readFile(path.join('public', 'data', 'dances.json'), function (err, obj) {
  * @param dance
  */
 function getSteps(dance) {
+
     let stepDb = "";
-    var steps = [];
     jf.readFile(path.join('public', 'data', 'filename_lookup.json'), function (err, obj) {
         if (obj[dance]) {
             stepDb = obj[dance];
             jf.readFile(path.join('public', 'data', stepDb), function (err, obj) {
                 if (obj) {
                     console.log(obj);
-                    steps = obj;
+                    return (obj);
                 }
                 else {
-                    console.log(err);
+                    return (err);
                 }
             });
         }
         else {
-            console.log("Step database not found for " + dance + ".\n" + err);
-            return [];
+            return (Error("Step database not found for " + dance + ".\n"));
         }
     });
-    return steps;
+    return [];
+
 }
+
 /* GET create page */
 router.get('/', function (req, res, next) {
     console.log(req.query['dance']);
-    var steps = getSteps(req.query['dance']);
-    console.log(steps);
-    res.render('create', {
-        title: 'Create Routine',
-        levels: ['Newcomer', 'Bronze', 'Silver', 'Gold', 'Open'],
-        categories: categories,
-        steps: steps
-    });
+    const dance = req.query['dance'];
+    if (dance) {
+        async.waterfall([
+            function (callback) {
+                let data = getSteps(dance);
+                callback(null, data);
+            }], function (err, result) {
+            console.log(result);
+            res.render('create', {
+                title: 'Create Routine',
+                levels: ['Newcomer', 'Bronze', 'Silver', 'Gold', 'Open'],
+                categories: categories,
+                steps: result
+            });
+        })
 
-});
+    }
+    else {
+        res.render('create', {
+            title: 'Create Routine',
+            levels: ['Newcomer', 'Bronze', 'Silver', 'Gold', 'Open'],
+            categories: categories,
+            steps: []
+        });
+    }
+
+})
+;
 
 router.get(':danceId', function (req, res, next) {
     console.log(req.params.danceId);
